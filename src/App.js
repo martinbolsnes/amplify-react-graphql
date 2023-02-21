1e4eb5da08f89450d89033ecf3e07144693c01e6
martinbolsnes <martinbolsnes@icloud.com>
2023-02-21 10:32:13 +0100

    changed styling

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Image,
   TextField,
   View,
+  Card,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
@@ -17,6 +18,28 @@ import {
   createNote as createNoteMutation,
   deleteNote as deleteNoteMutation,
 } from './graphql/mutations';
+import { BsPinAngleFill } from 'react-icons/bs';
+
+const css = `.custom-card-class {
+  background-color: #FFFF99;
+  position: relative;
+}
+.delete-btn {
+  color: red;
+  border: none;
+}
+
+.bg-image {
+  background-image: url('https://images.unsplash.com/photo-1558051815-0f18e64e6280?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80');
+  min-height: 100vh;
+  width: 100%;
+}
+
+.pin {
+  position: absolute;
+  top: -12px;
+  left: 50%;
+}`;
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -70,15 +93,20 @@ const App = ({ signOut }) => {
 
   return (
     <View className='App'>
-      <Heading level={1}>My Notes App</Heading>
+      <style>{css}</style>
+      <Flex direction='row' justifyContent='space-between' margin='2rem'>
+        <Heading level={1}>My Notes App</Heading>
+        <Button variation='link' onClick={signOut}>
+          Sign Out
+        </Button>
+      </Flex>
       <View as='form' margin='3rem 0' onSubmit={createNote}>
-        <Flex direction='row' justifyContent='center'>
+        <Flex direction='row' justifyContent='flex-start' margin='2rem'>
           <TextField
             name='name'
             placeholder='Note Name'
             label='Note Name'
             labelHidden
-            variation='quiet'
             required
           />
           <TextField
@@ -86,7 +114,6 @@ const App = ({ signOut }) => {
             placeholder='Note Description'
             label='Note Description'
             labelHidden
-            variation='quiet'
             required
           />
           <View
@@ -100,33 +127,52 @@ const App = ({ signOut }) => {
           </Button>
         </Flex>
       </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin='3rem 0'>
-        {notes.map((note) => (
+      <View as='div' className='bg-image' width='100%' height='100%'>
+        <View margin='1rem 0' paddingTop='2rem'>
           <Flex
-            key={note.id || note.name}
             direction='row'
-            justifyContent='center'
-            alignItems='center'
+            justifyContent='flex-start'
+            alignItems='stretch'
+            wrap='wrap'
+            gap='1rem'
+            margin='2rem'
           >
-            <Text as='strong' fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as='span'>{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
-              />
-            )}
-            <Button variation='link' onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
+            {notes.map((note) => (
+              <Flex key={note.id || note.name}>
+                <Card
+                  className='custom-card-class'
+                  variation='elevated'
+                  width={200}
+                  height='fit-content'
+                >
+                  <BsPinAngleFill className='pin' color='red' size={32} />
+                  <Flex direction='column' alignItems='flex-start'>
+                    <Text as='strong' fontWeight={700}>
+                      {note.name}
+                    </Text>
+                    <Text as='span'>{note.description}</Text>
+                    {note.image && (
+                      <Image
+                        src={note.image}
+                        alt={`visual aid for ${notes.name}`}
+                        width='50%'
+                      />
+                    )}
+                    <Button
+                      className='delete-btn'
+                      variation='warning'
+                      size='small'
+                      onClick={() => deleteNote(note)}
+                    >
+                      Delete note
+                    </Button>
+                  </Flex>
+                </Card>
+              </Flex>
+            ))}
           </Flex>
-        ))}
+        </View>
       </View>
-      <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
